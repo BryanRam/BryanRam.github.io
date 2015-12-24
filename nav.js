9f71c085b798941fa8b27032849f699b9c898af8
BryanRam <bryan_ram@hotmail.com>
2015-12-24 11:14:42 -0400

    moved javascript to own file

---
+++ nav.js
@@ -0,0 +1,19 @@
+$(function(){
+		  $("#includedContent").load("navigation-bar.html"); 
+		  var sPath = window.location.pathname;
+		  var sPage = sPath.substring(sPath.IndexOf('/'), sPath.lastIndexOf('/'));
+		  switch(sPage)
+		  {
+			case "index.html":
+			break;
+			
+			case "WebProjects":
+			var doc = document.getElementById("web");
+			doc.className = doc.className + " active";
+			break;
+			
+			case "Games":
+			var doc = document.getElementById("game");
+			doc.className = doc.className + " active";
+			break;
+		  }
