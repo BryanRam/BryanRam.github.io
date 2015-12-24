3639c8c6505b15adfd15959ee9ac8d648f12ecb7
BryanRam <bryan_ram@hotmail.com>
2015-12-24 12:03:01 -0400

    testing path again

--- nav.js
+++ nav.js
@@ -1,20 +1,4 @@
 $(function(){
 		  $("#includedContent").load("/navigation-bar.html"); 
-		  var sPath = window.location.pathname;
-		  var sPage = sPath.substring(sPath.indexOf('/'), sPath.lastIndexOf('/'));
-		  switch(sPage)
-		  {
-			case "index.html":
-			break;
-			
-			case "WebProjects":
-			var doc = document.getElementById("web");
-			doc.className = doc.className + " active";
-			break;
-			
-			case "Games":
-			var doc = document.getElementById("game");
-			doc.className = doc.className + " active";
-			break;
-		  }
+		  
 });
