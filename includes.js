e5732732bf44ba9890b7752e7c764161f359470f
BryanRam <bryan_ram@hotmail.com>
2016-01-16 18:34:02 -0400

    cleaning up code in background vid page

--- includes.js
+++ includes.js
@@ -5,10 +5,10 @@ function myFunction() {
 newscript.innerHTML + '\n' +
 '<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">' + '\n' 
 + '<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->' + '\n' +
-'<link href="dist/css/roboto.min.css" rel="stylesheet">' + '\n' +
-'<link href="dist/css/material.min.css" rel="stylesheet">' + '\n' +
-'<link href="dist/css/ripples.min.css" rel="stylesheet">' + '\n' +
-'<link href="dist/css/submenu.css" rel="stylesheet">'
+'<link href="/dist/css/roboto.min.css" rel="stylesheet">' + '\n' +
+'<link href="/dist/css/material.min.css" rel="stylesheet">' + '\n' +
+'<link href="/dist/css/ripples.min.css" rel="stylesheet">' + '\n' +
+'<link href="/dist/css/submenu.css" rel="stylesheet">'
 + '\n';
 }
 </script>
