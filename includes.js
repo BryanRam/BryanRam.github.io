a2502d3488138f0130526b47e0f4221d2bf8a934
BryanRam <bryan_ram@hotmail.com>
2016-01-16 21:46:10 -0400

    testing includes.js

--- includes.js
+++ includes.js
@@ -1,6 +1,5 @@
-function myFunction() {
-    var newscript = document.getElementsByTagName("head")[0];
-    newscript.innerHTML =
+var newscript = document.getElementsByTagName("head")[0];
+newscript.innerHTML =
 newscript.innerHTML + '\n' +
 '<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">' + '\n' 
 + '<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->' + '\n' +
@@ -9,4 +8,4 @@ newscript.innerHTML + '\n' +
 '<link href="/dist/css/ripples.min.css" rel="stylesheet">' + '\n' +
 '<link href="/dist/css/submenu.css" rel="stylesheet">'
 + '\n';
-}
+
