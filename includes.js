731d148ccfa721361c58d9d83e9bd3302f1e763f
BryanRam <bryan_ram@hotmail.com>
2016-01-24 22:08:22 -0400

    reordered argument in includes.js so the source pages' include links come last

--- includes.js
+++ includes.js
@@ -1,11 +1,11 @@
 var newscript = document.getElementsByTagName("head")[0];
 newscript.innerHTML =
-newscript.innerHTML + '\n' +
+'\n' +
 '<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">' + '\n' 
 + '<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->' + '\n' +
 '<link href="/dist/css/roboto.min.css" rel="stylesheet">' + '\n' +
 '<link href="/dist/css/material.min.css" rel="stylesheet">' + '\n' +
 '<link href="/dist/css/ripples.min.css" rel="stylesheet">' + '\n' +
-'<link href="/dist/css/submenu.css" rel="stylesheet">'
-+ '\n';
+'<link href="/dist/css/submenu.css" rel="stylesheet">' + '\n' +
+newscript.innerHTML;
 
